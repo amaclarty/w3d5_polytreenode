9bc2bcda38ea5a1fcaace8b1ddce46c6ce37fe01
“amaclarty” <andrewmaclarty@gmail.com>
2023-11-03 15:49:36 -0400

    almost done

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -40,11 +40,53 @@ class PolyTreeNode
 
     end    
 
+    def dfs(target_value)
+        return self if self.value == target_value 
+        self.children.each do |child|
+            result = child.dfs(target_value)
+            return result if result 
+        end
+        #return result if result
+        nil 
+    end
+
+    def bfs(target)
+        # return self if self.value == target
+        queue = []
+        queue.push(self)
+        queue.each do |node|
+            1st = queue.shift
+            return 1st if 1st.value == target 
+            node.child.bfs(target)
+        end    
+        nil     
+    end
+
+
+
+# target = e
+#   a
+#  b c
+#d e  f g 
+#
+#   
+#       self.bfs(e)
+#       queue = [a]
+#       a == e ?
+#       [b,c]
+#       call next bfs(b)
+#       [b]
+#
+#
+
+
+
+    attr_reader :parent, :children, :value
+end 
+
+# class Array 
     
 
-    # private 
-    attr_reader :parent, :children, :value
-
-end    
+# end    
 
 
