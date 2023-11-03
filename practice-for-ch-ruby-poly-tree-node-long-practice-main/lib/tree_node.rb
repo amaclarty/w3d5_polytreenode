16eba969e3c370303ffc7ca8ab68abe22f3485f5
“amaclarty” <andrewmaclarty@gmail.com>
2023-11-03 15:10:19 -0400

    tree node p1

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,50 @@
+require "Byebug"
+# debugger
 class PolyTreeNode
-  
-end
+    
+    def initialize(value)
+        @parent = nil 
+        @value = value 
+        @children = [] 
+    end
+
+    def parent=(new_parent)
+ 
+   
+        if self.parent && new_parent
+
+            self.parent.children.delete(self)
+            @parent = new_parent
+            
+            new_parent.children << self 
+        elsif !new_parent && self.parent 
+        
+            self.parent.children.delete(self)
+            @parent = new_parent
+        else
+            @parent = new_parent
+     
+            new_parent.children << self
+        end
+    end
+
+
+    def add_child(child)
+        # child.parent.children.delete(child) 
+        # children << child 
+        child.parent = self  
+    end   
+    
+    def remove_child(child)
+        child.parent=(nil)
+
+    end    
+
+    
+
+    # private 
+    attr_reader :parent, :children, :value
+
+end    
+
+
